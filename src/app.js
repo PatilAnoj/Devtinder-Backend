04dc23cb7071a8cc5e13f7f10055da434add24e8
Patil Anoj <anojpatil770@gmail.com>
2025-05-21 15:57:09 +0530

    initiated project

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const app=express();
+
+app.use("/test",(req,res)=>{
+    res.send("hello from the server test");
+});
+
+app.use("/",(req,res)=>{
+    res.send("hello from the server");
+});
+
+app.listen(3000,()=>{
+    console.log("server started on port 3000")
+})
