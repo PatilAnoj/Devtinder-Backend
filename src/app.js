9a58a8c249d57642a1a460e45109726c1bde047a
Patil Anoj <anojpatil770@gmail.com>
2025-05-22 19:15:54 +0530

    added http methods

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
 const express=require("express");
 const app=express();
 
-app.use("/test",(req,res)=>{
-    res.send("hello from the server test");
-});
+// app.use("/test",(req,res)=>{
+//     res.send("hello from the server test");
+// });
 
-app.use("/",(req,res)=>{
-    res.send("hello from the server");
-});
+// app.use("/",(req,res)=>{
+//     res.send("hello from the server");
+// });
+
+app.get("/user",(req,res)=>{
+    res.send({username:"Akshay",userid:"1234"})
+})
+
+app.post("/user",(req,res)=>{
+    res.send("added new user")
+})
+
+app.patch("/user",(req,res)=>{
+    res.send("user details updated");
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("user deleted");
+})
 
 app.listen(3000,()=>{
     console.log("server started on port 3000")
